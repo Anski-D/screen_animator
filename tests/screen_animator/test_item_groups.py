b7fddeff690b73c99cfe39d3a77064e450cf54e1
David Anscombe <dave.anski@gmail.com>
2022-08-27 18:55:44 +0100

    Update fixtures for testing item_groups module

--- tests/screen_animator/test_item_groups.py
+++ tests/screen_animator/test_item_groups.py
@@ -5,38 +5,39 @@ from screen_animator.settings import SettingsManager
 from screen_animator.item_groups import LeftScrollingTextGroup
 
 
+@pytest.fixture
+def example_settings_manager(monkeypatch, example_settings_dict_with_tuples):
+    pg.init()
+    monkeypatch.setattr(
+        SettingsManager,
+        "_import_settings",
+        lambda x, y: example_settings_dict_with_tuples,
+    )
+    return SettingsManager(None, None)
+
+
 class TestLeftScrollingTextGroup:
     @pytest.fixture
-    def example_left_scrolling_text(
-        self, monkeypatch, example_settings_dict_with_tuples, example_perimeter
-    ):
-        pg.init()
-        monkeypatch.setattr(
-            SettingsManager,
-            "_import_settings",
-            lambda x, y: example_settings_dict_with_tuples,
-        )
-        settings_manager = SettingsManager(None, None)
-
-        return LeftScrollingTextGroup(settings_manager.settings, example_perimeter)
+    def example_left_scrolling_text_group(self, example_settings_manager, example_perimeter):
+        return LeftScrollingTextGroup(example_settings_manager.settings, example_perimeter)
 
     @pytest.mark.parametrize("num_of_items", [1, 2, 3, 5, 8])
-    def test_create(self, num_of_items, example_left_scrolling_text):
-        item_group = example_left_scrolling_text
+    def test_create(self, num_of_items, example_left_scrolling_text_group):
+        item_group = example_left_scrolling_text_group
 
         for _ in range(num_of_items):
             item_group.create()
 
         assert len(item_group._group) == num_of_items
 
-    def test_create_position(self, example_left_scrolling_text, example_perimeter):
-        item_group = example_left_scrolling_text
+    def test_create_position(self, example_left_scrolling_text_group, example_perimeter):
+        item_group = example_left_scrolling_text_group
         item_group.create()
 
         assert item_group._group.sprites()[0].rect_box.left == example_perimeter.right
 
-    def test_update_create(self, example_left_scrolling_text):
-        item_group = example_left_scrolling_text
+    def test_update_create(self, example_left_scrolling_text_group):
+        item_group = example_left_scrolling_text_group
         item_group.create()
         item = item_group._group.sprites()[0]
         width = item.rect_box.width
@@ -47,8 +48,8 @@ class TestLeftScrollingTextGroup:
 
         assert len(item_group._group.sprites()) == 2
 
-    def test_update_delete(self, monkeypatch, example_left_scrolling_text):
-        item_group = example_left_scrolling_text
+    def test_update_delete(self, monkeypatch, example_left_scrolling_text_group):
+        item_group = example_left_scrolling_text_group
         item_group.create()
         item = item_group._group.sprites()[0]
         width = item.rect_box.width + item_group._perimeter.width
--- tests/screen_animator/test_settings.py
+++ tests/screen_animator/test_settings.py
@@ -29,7 +29,7 @@ class TestSettingsImporter:
 
 class TestSettingsManager:
     @pytest.fixture
-    def setup_settings_manager(self, monkeypatch, example_settings_dict_with_tuples):
+    def example_settings_manager(self, monkeypatch, example_settings_dict_with_tuples):
         pg.init()
         monkeypatch.setattr(
             SettingsManager,
@@ -41,8 +41,8 @@ class TestSettingsManager:
         return SettingsManager(None, None)
 
     @pytest.mark.parametrize("group", ["bg", "messages"])
-    def test_set_colors(self, group, setup_settings_manager):
-        settings_manager = setup_settings_manager
+    def test_set_colors(self, group, example_settings_manager):
+        settings_manager = example_settings_manager
         settings_manager.set_colors()
 
         assert (
@@ -50,8 +50,8 @@ class TestSettingsManager:
             in settings_manager.settings["colors"]
         )
 
-    def test_set_outline_color(self, setup_settings_manager):
-        settings_manager = setup_settings_manager
+    def test_set_outline_color(self, example_settings_manager):
+        settings_manager = example_settings_manager
         settings_manager.set_colors()
 
         assert (
@@ -59,30 +59,30 @@ class TestSettingsManager:
             in settings_manager.settings["messages"]["outline_colors"]
         )
 
-    def test_set_font(self, setup_settings_manager):
-        settings_manager = setup_settings_manager
+    def test_set_font(self, example_settings_manager):
+        settings_manager = example_settings_manager
         settings_manager._set_font()
 
         assert isinstance(settings_manager.settings["messages"]["font"], pg.font.Font)
 
     @pytest.mark.parametrize("repeat", range(5))
     def test_generate_text(
-        self, repeat, setup_settings_manager, example_settings_dict_with_tuples
+        self, repeat, example_settings_manager, example_settings_dict_with_tuples
     ):
-        settings_manager = setup_settings_manager
+        settings_manager = example_settings_manager
 
         assert settings_manager._generate_message_text() in [
             f'{message}{example_settings_dict_with_tuples["messages"]["separator"]}'
             for message in example_settings_dict_with_tuples["messages"]["messages"]
         ]
 
-    def test_generate_text_return_type(self, setup_settings_manager):
-        settings_manager = setup_settings_manager
+    def test_generate_text_return_type(self, example_settings_manager):
+        settings_manager = example_settings_manager
 
         assert isinstance(settings_manager._generate_message_text(), str)
 
-    def test_generate_message(self, setup_settings_manager):
-        settings_manager = setup_settings_manager
+    def test_generate_message(self, example_settings_manager):
+        settings_manager = example_settings_manager
         messages_dict = settings_manager._settings["messages"]
         messages_dict["color"] = [0, 0, 0]
         messages_dict["font"] = pg.font.Font(None, 10)
@@ -90,10 +90,10 @@ class TestSettingsManager:
         assert isinstance(settings_manager._generate_message(), pg.Surface)
 
     def test_load_images(
-        self, monkeypatch, setup_settings_manager, example_settings_dict_with_tuples
+        self, monkeypatch, example_settings_manager, example_settings_dict_with_tuples
     ):
         monkeypatch.setattr(pg.image, "load", lambda x: pg.Surface((20, 10)))
-        settings_manager = setup_settings_manager
+        settings_manager = example_settings_manager
         settings_manager._load_images()
         images_dict = settings_manager.settings["images"]
 
@@ -101,8 +101,8 @@ class TestSettingsManager:
             example_settings_dict_with_tuples["images"]["sources"]
         )
 
-    def test_load_images_file_not_found_error_not_raised(self, setup_settings_manager):
-        settings_manager = setup_settings_manager
+    def test_load_images_file_not_found_error_not_raised(self, example_settings_manager):
+        settings_manager = example_settings_manager
         try:
             settings_manager._load_images()
         except FileNotFoundError as error:
