e028f80d13be713094ee49ba7fc5e1a2da6577e5
David Anscombe <dave.anski@gmail.com>
2022-08-28 15:31:41 +0100

    Fill out some of Model class, add __init__ method to provide some parameter, and init method to actively set-up the instance

--- src/screen_animator/model.py
+++ src/screen_animator/model.py
@@ -1,2 +1,33 @@
+import pygame as pg
+from .settings import SettingsManager
+from .item_groups import ItemGroup
+
+
 class Model:
-    pass
+    def __init__(
+        self,
+        settings_manager: SettingsManager,
+        perimeter: pg.Rect,
+        item_group_types: list[type[ItemGroup]],
+    ) -> None:
+        self._settings_manager = settings_manager
+        self._perimeter = perimeter
+        self._item_groups = [
+            group(self._settings_manager.settings, self._perimeter)
+            for group in item_group_types
+        ]
+        self._initialized = False
+
+    @property
+    def initialized(self) -> bool:
+        return self._initialized
+
+    def init(self) -> None:
+        for item_group in self._item_groups:
+            item_group.create()
+
+        self._initialized = True
+
+    def update(self) -> None:
+        for item_group in self._item_groups:
+            item_group.update()
