7f4b34cd6303b518679f9c30e73d9a8846051b77
David Anscombe <dave.anski@gmail.com>
2022-09-05 20:32:06 +0100

    Create a manual initialisation method for Controller class

--- src/screen_animator/controller.py
+++ src/screen_animator/controller.py
@@ -8,3 +8,12 @@ class Controller:
         self._settings_manager = settings_manager
         self._model = model
         self._view = View(model, self, settings_manager.settings)
+        self._initialized = False
+
+    def init(self) -> None:
+        self._view.init()
+        self._model.init(self._view.perimeter)
+        self._model.add_observer(self._view)
+
+        if self._view.initialized and self._model.initialized:
+            self._initialized = True
