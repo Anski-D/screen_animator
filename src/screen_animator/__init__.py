c3f5c139dcfc560b385ee5f801febab6f9ca6224
David Anscombe <dave.anski@gmail.com>
2022-09-09 15:53:13 +0100

    Add top-level function to copy example files to working directory (for end user)

--- src/screen_animator/__init__.py
+++ src/screen_animator/__init__.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import pygame as pg
+import importlib.resources
+import shutil
 from .item_groups import (
     LeftScrollingTextGroup,
     ColorChangeGroup,
@@ -8,13 +10,17 @@ from .item_groups import (
 from .settings import SettingsManager, SettingsImporter
 from .model import Model
 from .controller import Controller
+import example
 
 __version__ = "0.1.0"
 
 item_groups = [LeftScrollingTextGroup, ColorChangeGroup, TimedRandomImagesGroup]
+example_files = ["inputs.toml", "script.py"]
 
 
 class ScreenAnimator:
+    _item_groups = item_groups
+
     def __init__(
         self,
         input_file: str | Path = "inputs.toml",
@@ -29,10 +35,17 @@ class ScreenAnimator:
     def run(self) -> None:
         pg.init()
         settings_manager = SettingsManager(SettingsImporter(), self._settings_file)
-        model = Model(settings_manager, item_groups)
+        model = Model(settings_manager, self._item_groups)
         controller = Controller(settings_manager, model, self._display_size)
         controller.init()
         controller.run()
 
     def _debug_setup(self) -> None:
         self._display_size = (800, 600)
+
+
+def copy_examples():
+    for file in example_files:
+        path = importlib.resources.path(example, file)
+        with path as p:
+            shutil.copy2(p, p.name)
