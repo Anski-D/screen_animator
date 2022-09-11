fc64fccaa0b529712e0df93f3939d954fd807bf7
David Anscombe <dave.anski@gmail.com>
2022-09-11 21:02:31 +0100

    Update example script.py

--- example/script.py
+++ example/script.py
@@ -1,5 +1,5 @@
 from screen_animator import ScreenAnimator
 
 if __name__ == "__main__":
-    screen_animator = ScreenAnimator(input_file="example/inputs.toml", debug=True)
+    screen_animator = ScreenAnimator(input_file="inputs.toml", debug=True)
     screen_animator.run()
