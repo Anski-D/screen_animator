185c05eff7a8624857773a3741f3772fd3e75c30
David Anscombe <dave.anski@gmail.com>
2022-09-15 21:16:17 +0100

    Add Model class documentation

--- src/screen_animator/model.py
+++ src/screen_animator/model.py
@@ -5,14 +5,42 @@ from .item_groups import ItemGroup
 
 
 class Model(Observable):
-    _perimeter: pg.Rect
+    """
+    Main engine for `screen_animator`.
+
+    Attributes
+    ----------
+    item_groups:
+        List of aspects to create and update.
+
+    Methods
+    -------
+    init
+        Manually perform some initialization.
+    update
+        Update the model.
+    quit
+        Set the model ready to quit.
+    """
+
     item_groups: list[ItemGroup]
+    _perimeter: pg.Rect
 
     def __init__(
         self,
         settings_manager: SettingsManager,
         item_group_types: list[type[ItemGroup]],
     ) -> None:
+        """
+        Set-up some initial parameters for the model.
+
+        Parameters
+        ----------
+        settings_manager
+            Manage the settings.
+        item_group_types
+            Class handles for elements of the model.
+        """
         super().__init__()
 
         self._settings_manager = settings_manager
@@ -21,9 +49,18 @@ class Model(Observable):
 
     @property
     def initialized(self) -> bool:
+        """Is model ready to use."""
         return self._initialized
 
     def init(self, perimeter: pg.Rect) -> None:
+        """
+        Manually finish initializing the display.
+
+        Parameters
+        ----------
+        perimeter
+            The outer limits in which the model can operate.
+        """
         self._perimeter = perimeter
         self.item_groups = [
             group(self._settings_manager, self._perimeter)
@@ -35,10 +72,12 @@ class Model(Observable):
         self._initialized = True
 
     def update(self) -> None:
+        """Update all aspects of the model."""
         for item_group in self.item_groups:
             item_group.update()
 
         self.notify_observers()
 
     def quit(self) -> None:
+        """Set the model as ready to quit."""
         self._initialized = False
