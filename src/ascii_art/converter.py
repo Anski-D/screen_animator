0b1fb20459bb6ed325fddf8af345f16a1a4a91a3
David Anscombe <dave.anski@gmail.com>
2022-09-20 17:26:57 +0100

    Implement method to calculate average luminance in Converter class

--- src/ascii_art/converter.py
+++ src/ascii_art/converter.py
@@ -1,5 +1,13 @@
+import numpy as np
 from PIL import Image
+
+g_scale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 
 
 class Converter:
-    pass
+    def __init__(self, image_file: str) -> None:
+        pass
+
+    def calculate_lum(self, image: Image) -> float:
+        im = np.array(image)
+        return np.average(im)
