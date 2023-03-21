3e551b755d102bb7960c769153ddae1e4540be91
David Anscombe <dave.anski@gmail.com>
2023-03-21 17:07:59 +0000

    Add additional parameters for customizing logging to file

--- src/screen_animator/log_setup.py
+++ src/screen_animator/log_setup.py
@@ -4,11 +4,15 @@ import logging.handlers
 LOG_FILE = "screen_animator.log"
 
 
-def setup_logging(logging_level="WARNING"):
+def setup_logging(logging_level="WARNING", size_mb: int = 20, bkp_count: int = 5):
     log = logging.getLogger(__package__)
     log.setLevel(getattr(logging, logging_level.upper()))
-    file_handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=2000000, backupCount=5)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler = logging.handlers.RotatingFileHandler(
+        LOG_FILE, maxBytes=size_mb * 10**6, backupCount=bkp_count
+    )
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     file_handler.setFormatter(formatter)
     log.addHandler(file_handler)
     stream_handler = logging.StreamHandler()
@@ -16,4 +20,8 @@ def setup_logging(logging_level="WARNING"):
     log.addHandler(stream_handler)
 
     log.info(50 * "=")
-    log.info("Logging to %s at %s level", LOG_FILE, logging.getLevelName(log.getEffectiveLevel()))
+    log.info(
+        "Logging to %s at %s level",
+        LOG_FILE,
+        logging.getLevelName(log.getEffectiveLevel()),
+    )
