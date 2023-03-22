1f22b5a13fad07002469dfc3cbca67d5bb54e209
David Anscombe <dave.anski@gmail.com>
2023-03-22 18:43:49 +0000

    Add test to make sure the ImageLoader class uses the correct function to load image type

--- tests/screen_animator/test_image_loading.py
+++ tests/screen_animator/test_image_loading.py
@@ -1,4 +1,34 @@
-from screen_animator.image_loading import load_raster_image, load_svg_image
+import pytest
+
+import screen_animator.image_loading
+from screen_animator.image_loading import ImageLoader, load_raster_image, load_svg_image
+
+
+class TestImageLoader:
+    @pytest.mark.parametrize(
+        "input_file, output",
+        [
+            ("test1.bmp", "raster"),
+            ("test2.png", "raster"),
+            ("test3.svg", "svg"),
+            ("test4.svg", "svg"),
+        ],
+    )
+    @pytest.mark.xfail
+    def test_load_image_file_type(
+        self, input_file: str, output: str, monkeypatch
+    ) -> None:
+        """Correct loader function is used for loading images."""
+        monkeypatch.setattr(
+            screen_animator.image_loading, "load_raster_image", lambda x, y: "raster"
+        )
+        monkeypatch.setattr(
+            screen_animator.image_loading, "load_svg_image", lambda x, y: "svg"
+        )
+        image_loader = ImageLoader()
+        image = image_loader.load_image(input_file, -1)
+
+        assert image == output
 
 
 def test_load_raster_image_file_not_found() -> None:
