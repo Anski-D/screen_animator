0c9fd540b216047de755c6caccc46682b9b1edd9
Dave Anscombe <dave.anski@gmail.com>
2024-03-08 21:42:17 +0000

    Update example script.py to make it clearer how the main class arguments can be set

--- example/script.py
+++ example/script.py
@@ -1,6 +1,16 @@
 from screen_animator import ScreenAnimator, log_setup
 
+INPUT_FILE = "inputs.toml"
+FLIPPED = False
+FPS_ON = False
+DEBUG = False
+
 if __name__ == "__main__":
     # log_setup.setup_logging(logging_level="DEBUG")
-    screen_animator = ScreenAnimator(input_file="inputs.toml", debug=True)
+    screen_animator = ScreenAnimator(
+        input_file=INPUT_FILE,
+        flipped=FLIPPED,
+        fps_on=FPS_ON,
+        debug=DEBUG,
+    )
     screen_animator.run()
