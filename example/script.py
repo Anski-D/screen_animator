725f9570b0d44d6f69f9dd9f81f032896d1a709a
Dave Anscombe <dave.anski@gmail.com>
2024-03-09 10:54:05 +0000

    Tweak example script.py to be in debugging mode by default

--- example/script.py
+++ example/script.py
@@ -3,10 +3,11 @@ from screen_animator import ScreenAnimator, log_setup
 INPUT_FILE = "inputs.toml"
 FLIPPED = False
 FPS_ON = False
-DEBUG = False
+DEBUG = True
+LOGGING_LEVEL = "DEBUG" if DEBUG else "INFO"
 
 if __name__ == "__main__":
-    # log_setup.setup_logging(logging_level="DEBUG")
+    log_setup.setup_logging(logging_level=LOGGING_LEVEL)
     screen_animator = ScreenAnimator(
         input_file=INPUT_FILE,
         flipped=FLIPPED,
