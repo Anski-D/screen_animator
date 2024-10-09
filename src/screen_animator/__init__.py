b7eecaa0d5b768db0200c754ef1131c1d06ec02e
Dave Anscombe <dave.anski@gmail.com>
2024-10-09 19:44:33 +0100

    Use `argparse` library to provide inputs into app

--- src/screen_animator/__init__.py
+++ src/screen_animator/__init__.py
@@ -1,6 +1,7 @@
 import importlib.resources
 import shutil
 from functools import partial
+import argparse
 
 import example
 from screen_animator.controller import Controller
@@ -20,102 +21,6 @@ from screen_animator.sa import ScreenAnimator
 
 EXAMPLE_FILES = ["inputs.toml", "script.py"]
 
-# log = logging.getLogger(__name__)
-
-
-# class ScreenAnimator:
-#     """
-#     Bring together models of `screen_animator` and run.
-#
-#     Methods
-#     -------
-#     register_item_group
-#         Add `ItemGroup` to class list.
-#     run
-#         Run `screen_animator`.
-#     """
-#
-#     _item_groups: list[type[ItemGroup]] = []
-#
-#     def __init__(
-#         self,
-#         input_file: str | Path = "inputs.toml",
-#         display_size: tuple[int, int] | None = None,
-#         flipped: bool = False,
-#         fps_on: bool = False,
-#         debug: bool = False,
-#     ) -> None:
-#         """
-#         Perform setup of `screen_animator`.
-#
-#         Parameters
-#         ----------
-#         input_file
-#             File with user settings, default is `inputs.toml` in working directory.
-#         display_size : optional
-#             User-defined screen size (default is None, full-screen).
-#         flipped : optional
-#             Flip display across the horizontal axis (default is False).
-#         fps_on
-#             FPS counter is on (default is False).
-#         debug
-#             Turns on debug model (default is False).
-#         """
-#         self._settings_file = input_file
-#         self._display_size = display_size
-#         log.info(
-#             "Display size set to: %s",
-#             "fullscreen" if self._display_size is None else self._display_size,
-#         )
-#         self._flipped = flipped
-#         log.info("Output will be flipped vertically: %s", self._flipped)
-#         self._is_fps_on = fps_on
-#         if self._is_fps_on:
-#             self._fps_on()
-#         self._debug = debug
-#         if self._debug:
-#             self._debug_setup()
-#         log.info("Creating %s", self)
-#
-#     def __repr__(self) -> str:
-#         return (
-#             f"{type(self).__name__}("
-#             f"{self._settings_file},"
-#             f" {self._display_size},"
-#             f" {self._flipped},"
-#             f" {self._is_fps_on},"
-#             f" {self._debug})"
-#         )
-#
-#     @classmethod
-#     def register_item_group(cls, item_group: type[ItemGroup]) -> None:
-#         """Add `ItemGroup` to class list."""
-#         cls._item_groups.append(item_group)
-#
-#     def run(self) -> None:
-#         """Run `screen_animator`."""
-#         log.info("Creating key components of %s", type(self).__name__)
-#         pg.init()  # pylint: disable=no-member
-#         settings_manager = SettingsManager(self._settings_file)
-#         settings_manager.setup_settings()
-#         model = Model(settings_manager, self._item_groups)
-#         controller = Controller(settings_manager, model, self._flipped)
-#         controller.init(self._display_size)
-#         log.info("Setting %s to run", type(self).__name__)
-#         controller.run()
-#         log.info("Run method complete, %s stopping", type(self).__name__)
-#
-#     def _debug_setup(self) -> None:
-#         log.info("Debug mode enabled")
-#         self._display_size = (800, 480)
-#         log.info("Display size changed to: %s", self._display_size)
-#         self._fps_on()
-#
-#     def _fps_on(self) -> None:
-#         log.info("FPS will be displayed")
-#         if FpsCounterItemGroup not in self._item_groups:
-#             self._item_groups.append(FpsCounterItemGroup)
-
 
 def copy_examples() -> None:
     """Copies example files to working directory."""
@@ -125,9 +30,48 @@ def copy_examples() -> None:
             shutil.copy2(path, path.name)
 
 
-def main() -> None:
-    setup_logging(logging_level="INFO")
+def parse_args() -> argparse.Namespace:
+    """Processes the command line arguments provided."""
+    parser = argparse.ArgumentParser(
+        prog="ScreenAnimator",
+        description="A Python app to generate animated messages and images on a screen, with a focus on the Raspberry Pi.",
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        default="inputs.toml",
+        help="path to TOML file with inputs (optional, falls back to `inputs.toml` by default)",
+    )
+    parser.add_argument(
+        "-r",
+        "--rotate",
+        action="store_true",
+        help="rotate rendered output by 180 degrees (optional, disabled by default)",
+    )
+    parser.add_argument(
+        "-f",
+        "--fps",
+        action="store_true",
+        help="turn on FPS counter (optional, off by default, forced on if debug mode used)",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="turns on debug mode which limits output to windowed mode, increases logging, and renders FPS counter (optional, off by default)",
+    )
+    parser.add_argument(
+        "-l",
+        "--logging",
+        default="",
+        help="activates logging and sets logging level (off by default, writes to log file when on)",
+    )
 
+    return parser.parse_args()
+
+
+def main() -> None:
+    """Main app function to run."""
     ImageLoader.register_loader(".svg", SvgTypeImageLoader)
 
     groups = [
@@ -139,11 +83,14 @@ def main() -> None:
     for group in groups:
         ScreenAnimator.register_item_group(group)  # type: ignore
 
+    args = parse_args()
+    setup_logging(args.logging)
+
     screen_animator = ScreenAnimator(
-        input_file="example/inputs.toml",
-        flipped=False,
-        fps_on=False,
-        debug=False,
+        input_file=args.input,
+        flipped=args.rotate,
+        fps_on=args.fps,
+        debug=args.debug,
     )
     screen_animator.run()
 
--- src/screen_animator/log_setup.py
+++ src/screen_animator/log_setup.py
@@ -6,24 +6,28 @@ LOG_DIR = "logs"
 LOG_FILE = f"{__package__}_{datetime.now().strftime('%Y%m%d%H%M')}.log"
 
 
-def setup_logging(logging_level="WARNING"):
+def setup_logging(logging_level):
     log = logging.getLogger(__package__)
-    log.setLevel(getattr(logging, logging_level.upper()))
-    Path(LOG_DIR).mkdir(exist_ok=True)
-    log_path = Path(LOG_DIR, LOG_FILE)
-    file_handler = logging.FileHandler(log_path)
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    file_handler.setFormatter(formatter)
-    log.addHandler(file_handler)
+
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.WARNING)
     log.addHandler(stream_handler)
 
-    log.info(50 * "=")
-    log.info(
-        "Logging to %s at %s level",
-        log_path,
-        logging.getLevelName(log.getEffectiveLevel()),
-    )
+    logging_level = logging_level.upper()
+    if logging_level in logging.getLevelNamesMapping():
+        log.setLevel(logging_level)
+        Path(LOG_DIR).mkdir(exist_ok=True)
+        log_path = Path(LOG_DIR, LOG_FILE)
+        file_handler = logging.FileHandler(log_path)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        file_handler.setFormatter(formatter)
+        log.addHandler(file_handler)
+
+        log.info(50 * "=")
+        log.info(
+            "Logging to %s at %s level",
+            log_path,
+            logging.getLevelName(log.getEffectiveLevel()),
+        )
